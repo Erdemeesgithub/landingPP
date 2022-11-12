1bf29096dffff48cca460e83f84e1456d3bdf459
22PHOP2D-Erdemee <erdemee@LEAP-04s-iMac.local>
2022-11-12 14:39:41 +0800

    11.12

--- page/src/components/Landing.js
+++ page/src/components/Landing.js
@@ -1,13 +1,53 @@
 import styles from "../styles/components/Landing.module.css";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import meet from "../assets/meet.png";
+import icon from "../assets/icon.png";
+import event from "../assets/event.png";
+import group from "../assets/Group.png";
+import { useEffect } from "react";
+import axios from "axios";
+
+const BaseUrl = "https://dummyapi.io/data/v1/";
 
 export const Landing = () => {
+  useEffect(() => {
+    axios
+      .get(BaseUrl + "user", {
+        headers: {
+          "app-id": "636f2fc0e8d0ffd60f3fc549",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className={styles.Container}>
       <div className={styles.header}>
         <div></div>
       </div>
-      <div className={styles.postPage1}></div>
+      <div className={styles.postPage1}>
+        <div className={styles.postPage1div}>
+          <p className={styles.text2}>Your Hub for teamwork</p>
+          <p className={styles.text3}>
+            Give everyone you work with—inside and outside your company—a more
+            productive way to stay in sync. Respond faster with emoji, keep
+            conversations focused in channels, and simplify all your
+            communication into one place.
+          </p>
+          <Link>Learn more</Link>
+        </div>
+        <div>
+          <img className={styles.meet} src={meet}></img>
+          <img className={styles.icon} src={icon}></img>
+          <img className={styles.event} src={event}></img>
+          <img className={styles.group} src={group}></img>
+        </div>
+      </div>
       <div className={styles.postPage2}></div>
       <div className={styles.postPage3}></div>
       <div className={styles.aboutUs}></div>
--- page/src/components/Page.js
+++ page/src/components/Page.js
@@ -1,47 +1,80 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { Button, Stack } from "react-bootstrap";
 import styles from "../styles/components/Page.module.css";
 import image from "../assets/Ellipse.png";
 import Rectangle from "../assets/Rectangle.png";
 import { useContext } from "react";
 import { ColorModeContext } from "../Context";
+import { useState, useEffect } from "react";
+import { Button, Card, Container, Spinner } from "react-bootstrap";
+import axios from "axios";
+
+const baseUrl = "https://dummyapi.io/data/v1/";
 
 export const Page = () => {
   const color = useContext(ColorModeContext);
-  const array = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  // const array = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(baseUrl + "user", {
+        headers: {
+          "app-id": "636f2f24e8d0ffd9c83fc52f",
+        },
+      })
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div className={styles.Container1}>
-      <div className={styles.All}>
-        <div className={styles.Texts}>
-          <h1>Blog Posts</h1>
-          <p className={styles.Textss}>
-            Our latest updates and blogs about managing your team
-          </p>
-          <div className={styles.card9}>
-            {array.map(() => {
-              return (
-                <div className={styles.display}>
-                  <img src={Rectangle} className={styles.img1}></img>
-                  <div className={styles.container}>
-                    <h2>Data-Driven Design is Killing Our Instincts</h2>
-                    <p>
-                      Our latest updates and blogs about managing your team Our
-                      latest updates and blogs about managing your team
-                    </p>
-                    <div className={styles.information}>
-                      <img src={image}></img>
-                      <p>Jane Cooper | 2nd January,2022</p>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <button className={styles.btn}>Next </button>
-      </div>
+    // <div className={styles.Container1}>
+    //   <div className={styles.All}>
+    //     <div className={styles.Texts}>
+    //       <h1>Blog Posts</h1>
+    //       <p className={styles.Textss}>
+    //         Our latest updates and blogs about managing your team
+    //       </p>
+    //       <div className={styles.card9}>
+    //         {array.map(() => {
+    //           return (
+    //             <div className={styles.display}>
+    //               <img src={Rectangle} className={styles.img1}></img>
+    //               <div className={styles.container}>
+    //                 <h2>Data-Driven Design is Killing Our Instincts</h2>
+    //                 <p>
+    //                   Our latest updates and blogs about managing your team Our
+    //                   latest updates and blogs about managing your team
+    //                 </p>
+    //                 <div className={styles.information}>
+    //                   <img src={image}></img>
+    //                   <p>Jane Cooper | 2nd January,2022</p>
+    //                 </div>
+    //               </div>
+    //             </div>
+    //           );
+    //         })}
+    //       </div>
+    //     </div>
+    //     <button className={styles.btn}>Next </button>
+    //   </div>
+    // </div>
+    <div>
+      {!data && <Spinner />}
+      {data && data.data.map((item) => <Item {...item} />)}
     </div>
+  );
+};
+const Item = (props) => {
+  return (
+    <Card style={{ width: 300, marginRight: 16 }}>
+      <Card.Body>
+        <Card.Title>{props.firstName}</Card.Title>
+        <Card.Text>{props.text}</Card.Text>
+        <Button variant="primary">{props.buttonText}</Button>
+      </Card.Body>
+    </Card>
   );
 };
